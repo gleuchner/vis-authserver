04adfbb36c6158f960c16dd19d3f0a795d89d6e4
gerrit <gerrit.leuchner@gmail.com>
2017-06-22 13:11:53 +0200

    implement userdetailsservice

--- src/main/java/de/hska/muon/HelloOauthSrvApplication.java
+++ src/main/java/de/hska/muon/HelloOauthSrvApplication.java
@@ -1,13 +1,52 @@
 package de.hska.muon;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class HelloOauthSrvApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloOauthSrvApplication.class, args);
 	}
 
+	@Value("${OAuth2ClientId:my-client-with-secret}")
+	private String oAuth2ClientId;
+
+	@Value("${OAuth2ClientSecret:secret}")
+	private String oAuth2ClientSecret;
+
+	@Value("${oauth.token:http://authserver:8763/oauth/token}")
+	private String accessTokenUri;
+
+	@Bean
+	public RestTemplate oAuthRestTemplate() {
+		ClientCredentialsResourceDetails resourceDetails = new ClientCredentialsResourceDetails();
+		resourceDetails.setId("1");
+		resourceDetails.setClientId(oAuth2ClientId);
+		resourceDetails.setClientSecret(oAuth2ClientSecret);
+		resourceDetails.setAccessTokenUri(accessTokenUri);
+		List<String> scopes = new ArrayList<>();
+		scopes.add("read");
+		scopes.add("write");
+		scopes.add("trust");
+		resourceDetails.setScope(scopes);
+
+		OAuth2RestTemplate restTemplate = new OAuth2RestTemplate(resourceDetails, new DefaultOAuth2ClientContext());
+
+		return restTemplate;
+	}
 }
--- src/main/java/de/hska/muon/UserDetailsServiceImpl.java
+++ src/main/java/de/hska/muon/UserDetailsServiceImpl.java
@@ -1,11 +1,13 @@
 package de.hska.muon;
 
-import ch.qos.logback.core.net.SyslogOutputStream;
+import de.hska.muon.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -15,44 +17,72 @@ import java.util.Collection;
  */
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
+    private static final String USER_SERVICE_URL = "http://user-service/users";
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public UserDetailsServiceImpl(final RestTemplate oAuthRestTemplate) {
+        this.restTemplate = oAuthRestTemplate;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        System.out.println("Userdetailsservice");
-        return new UserDetails() {
-            @Override
-            public Collection<? extends GrantedAuthority> getAuthorities() {
-                return new ArrayList<>();
+        User[] users = getUsers();
+        for (User user :users) {
+            if (user.getUsername().equals(username)) {
+                return new UserDetailsImpl(user);
             }
+        }
 
-            @Override
-            public String getPassword() {
-                return "test";
-            }
+        throw new UsernameNotFoundException("User with name \"" + username + "\" not found");
+    }
 
-            @Override
-            public String getUsername() {
-                return "test";
-            }
+    private User[] getUsers() {
+        return restTemplate.getForObject(USER_SERVICE_URL, User[].class);
+    }
 
-            @Override
-            public boolean isAccountNonExpired() {
-                return true;
-            }
+    class UserDetailsImpl implements UserDetails {
 
-            @Override
-            public boolean isAccountNonLocked() {
-                return true;
-            }
+        private User _user;
 
-            @Override
-            public boolean isCredentialsNonExpired() {
-                return true;
-            }
+        public UserDetailsImpl(User user) {
+            _user = user;
+        }
 
-            @Override
-            public boolean isEnabled() {
-                return true;
-            }
-        };
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            return new ArrayList<>();
+        }
+
+        @Override
+        public String getPassword() {
+            return _user.getPassword();
+        }
+
+        @Override
+        public String getUsername() {
+            return _user.getUsername();
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+            return true;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return true;
+        }
     }
 }
